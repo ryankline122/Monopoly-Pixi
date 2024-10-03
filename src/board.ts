9bb72d6797596fe5ca7772f76965e8795b1546a9
Ryan Kline <ryanjkline@protonmail.com>
2024-10-02 21:28:46 -0400

    Clean up board creation

--- src/board.ts
+++ src/board.ts
@@ -1,13 +1,13 @@
 import { Container, Graphics } from "pixi.js";
 import { Sides } from "./enums/sides";
-import { Shop } from "./shop";
-import { Shops } from "./constants/shops";
+import { Space } from "./space";
+import { Spaces } from "./constants/spaces";
 
 export class Board {
   private width: number = 904;
   private height: number = 904;
   private boardContainer: Container;
-  private shops: Shop[] = Shops;
+  private spaces: Space[] = Spaces;
 
   public constructor() {
     this.boardContainer = this.createBoard();
@@ -27,65 +27,57 @@ export class Board {
     boardContainer.pivot.set(board.width / 2, board.height / 2);
     boardContainer.label = "board";
 
-    const corners = this.createCorners();
-    for (let i = 0; i < corners.length; i++) {
-      boardContainer.addChild(corners[i]);
-    }
-
-    const shops = this.createShops();
-    for (let i = 0; i < shops.length; i++) {
-      boardContainer.addChild(shops[i]);
-    }
+    this.createSpaces().forEach((c) => {
+      boardContainer.addChild(c);
+    });
 
     return boardContainer;
   }
 
-  private createCorners() {
-    const freeParking = this.createCorner(0, 0);
-    const goToJail = this.createCorner(this.width, 0);
-    const go = this.createCorner(this.width, this.height);
-    const jail = this.createCorner(0, this.height);
+  private createSpaces(): Container[] {
+    const corners = [0, 10, 20, 30];
+    const spaceContainers: Container[] = [];
 
-    freeParking.label = "FreeParking";
-    goToJail.label = "GoToJail";
-    go.label = "Go";
-    jail.label = "Jail";
+    this.spaces.forEach((s) => {
+      if (!corners.includes(s.index)) {
+        // Shops
+        const container = new Container();
+        const shopGraphic = new Graphics()
+          .rect(s.x, s.y, s.width, s.height)
+          .fill({
+            color: 0xffffff,
+          })
+          .stroke({
+            width: 2,
+            color: 0x0000000,
+          });
+        container.label = s.name;
+        container.addChild(shopGraphic);
+        container.addChild(this.addShopColor(s));
+        spaceContainers.push(container);
+      } else {
+        // Corners 
+        const container = new Container();
+        const corner = new Graphics()
+          .rect(s.x, s.y, s.width, s.width)
+          .stroke({
+            width: 1,
+            color: 0x0000000,
+          });
 
-    return [
-      freeParking,
-      goToJail,
-      go,
-      jail
-    ];
+        container.label = s.name;
+        container.addChild(corner);
+        spaceContainers.push(container);
+      }
+    });
+
+    return spaceContainers;
   }
 
-  private createShops() {
-    const shops: Container[] = [];
-
-    for (let i = 0; i < this.shops.length; i++) {
-      shops.push(this.createShop(this.shops[i]));
-    }
-
-    return shops;
-  }
-
-  private createShop(shop: Shop) {
-    const isHorizontal = shop.side == Sides.Top || shop.side == Sides.Bottom;
-    const shopContainer = new Container();
-    const shopGraphic = new Graphics()
-      .rect(shop.x, shop.y, shop.width, shop.height)
-      .fill({
-        color: 0xffffff,
-      })
-      .stroke({
-        width: 2,
-        color: 0x0000000,
-      });
-    shopContainer.label = shop.name;
-    shopContainer.addChild(shopGraphic);
-
+  private addShopColor(shop: Space): Graphics {
     let colorX = shop.x;
     let colorY = shop.y;
+    const isHorizontal = shop.side == Sides.Top || shop.side == Sides.Bottom;
     const colorWidth = isHorizontal ? 72 : 36;
     const colorHeight = isHorizontal ? 36: 72;
 
@@ -98,7 +90,7 @@ export class Board {
         break;
     }
 
-    const shopColor = new Graphics()
+    return new Graphics()
       .rect(colorX, colorY, colorWidth, colorHeight)
       .stroke({
         width: 2,
@@ -107,26 +99,5 @@ export class Board {
       .fill({
         color: shop.color,
       });
-
-    shopContainer.addChild(shopColor);
-
-    return shopContainer;
-  }
-
-  private createCorner(x: number, y: number) {
-    const container = new Container();
-    const cornerSize = 128;
-    const width = x > 0 ? x - cornerSize : x;
-    const height = y > 0 ? y - cornerSize : y;
-    const corner = new Graphics()
-      .rect(width, height, cornerSize, cornerSize)
-      .stroke({
-        width: 1,
-        color: 0x0000000,
-      });
-
-    container.addChild(corner);
-
-    return container;
   }
 }
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-var */
 declare global {
     var __PIXI_APP__: any;
   }
